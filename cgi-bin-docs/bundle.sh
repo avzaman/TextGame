e1ab2b3e6e1022f9e0d354510ea87b2f92268d0a
avzaman <vin.zaman@gmail.com>
2024-04-24 15:23:28 -0400

    fixed bundle for having repo on obi

--- cgi-bin-docs/bundle.sh
+++ cgi-bin-docs/bundle.sh
@@ -3,3 +3,8 @@ g++ -c Game.cpp -std=c++11
 g++ -c main.cpp  -std=c++11
 g++ -o project3 main.o Game.o ProcessCgi.o -lcgicc
 chmod 705 project3
+chmod 705 ../project3.php
+cp -fi project3 ../../cgi-bin
+cp -fi ProcessCgi.h ../../cgi-bin
+cp -fi Game.h ../../cgi-bin
+cp -fi ../project3.php ../../CPS3525
