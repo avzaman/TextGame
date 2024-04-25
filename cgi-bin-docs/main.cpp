0cdba6f3f65f562e43e818d98bf6afe8e21734c5
Iramir Neto <70657151+iramirneto@users.noreply.github.com>
2024-04-24 23:41:13 -0400

    Add files via upload

--- cgi-bin-docs/main.cpp
+++ cgi-bin-docs/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdlib>
 
 #include "Game.h"
 #include "ProcessCgi.h"
@@ -21,16 +22,34 @@ int main()
     try
     {
         ProcessCgi proc; // Attempt to construct ProcessCgi object
-        cout << "<br> made it out of cons";
+        cout << "<br> User's Input:";
 
         cout << "<br>Chosen char: " << proc.getChosen();
         cout << "<br>Count int: " << proc.getCount();
         cout << "<br>Text string:<br>" << proc.getText();
+
+        Game game; //Crete a Game object and process inputs
+
+        //Get the results from the Game class
+        int results = game.getResults();
+
+        if(results == 0){
+            cout << "<br> You guessed the right number!!";
+        }else if(results > 0){
+            cout << "<br> Your guess is too high";
+        }else{
+            cout << "<br> Your guess is too low";
+
     }
     catch (const std::exception &e)
     {
-        cout << "Exception caught during ProcessCgi construction: " << e.what() << endl;
+        cout << "Exception caught during ProcessCgi or Game construction: " << e.what() << endl;
         exit(1);
     }
+
+    cout << "</body>\n";
+    cout << "</html>\n";
+
+    return 0;
+    }
 }
-// I made a temporary main function to test out ProcessCgi class and compile script <3 -Vince
