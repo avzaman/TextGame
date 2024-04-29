4bc5a5fa1537ff771c89909cf480bedc18b661e0
avzaman <vin.zaman@gmail.com>
2024-04-29 13:25:55 -0400

    fixed bundle

--- cgi-bin-docs/bundle.sh
+++ cgi-bin-docs/bundle.sh
@@ -3,8 +3,8 @@ g++ -c Game.cpp -std=c++11
 g++ -c main.cpp  -std=c++11
 g++ -o project3 main.o Game.o ProcessCgi.o -lcgicc
 chmod 705 project3
-chmod 705 ../project3.php
-cp -fi project3 ../../cgi-bin
-cp -fi ProcessCgi.h ../../cgi-bin
-cp -fi Game.h ../../cgi-bin
-cp -fi ../project3.php ../../CPS3525
+chmod 705 ../public-html-docs/project3.php
+cp -fi project3 ../../../cgi-bin/project3
+cp -fi ProcessCgi.h ../../../cgi-bin/ProcessCgi.h
+cp -fi Game.h ../../../cgi-bin/Game.h
+cp -fi ../public-html-docs/project3.php ../../project3.php
