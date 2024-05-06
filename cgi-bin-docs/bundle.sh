4e010cea07de24463d9f46f393aa599c28839060
avzaman <vin.zaman@gmail.com>
2024-05-06 15:01:44 -0400

    Vince - fixed bundle permission and force

--- cgi-bin-docs/bundle.sh
+++ cgi-bin-docs/bundle.sh
@@ -5,8 +5,8 @@ g++ -o project3 main.o Game.o ProcessCgi.o -lcgicc
 chmod 705 project3
 chmod 705 ../public-html-docs/project3.php
 chmod 705 ../public-html-docs/style.css
-cp -fi project3 ../../../cgi-bin/project3
-cp -fi ProcessCgi.h ../../../cgi-bin/ProcessCgi.h
-cp -fi Game.h ../../../cgi-bin/Game.h
-cp -fi ../public-html-docs/project3.php ../../project3.php
-cp -fi ../public-html-docs/style.css ../../style.css
+cp -fp project3 ../../../cgi-bin/project3
+cp -fp ProcessCgi.h ../../../cgi-bin/ProcessCgi.h
+cp -fp Game.h ../../../cgi-bin/Game.h
+cp -fp ../public-html-docs/project3.php ../../project3.php
+cp -fp ../public-html-docs/style.css ../../style.css
